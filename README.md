69cf6e1349e0f3250e48238b449519e84a326b01
wangfucheng <wangfucheng@machloop.com>
2021-03-16 12:38:37 +0800

    feat: ok

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # bing-daily-wallpaper
 
 使用 `deno`、`Github Actions` 自动抓取 `Bing` 搜索每天的首页背景图。
+
+<!-- BEGIN -->
+
+<!-- END -->
--- types.ts
+++ types.ts
@@ -1,19 +1,24 @@
 export interface IImg {
-  startdate: string;
-  fullstartdate: string;
-  enddate: string;
   url: string;
-  urlbase: string;
+  previewUrl: string;
+  date: string;
   copyright: string;
-  copyrightlink: string;
-  title: string;
-  quiz: string;
-  wp: boolean;
-  hsh: string;
-  [key: string]: any;
 }
 
 export interface IResponse {
-  images: IImg[];
+  images: {
+    startdate: string;
+    fullstartdate: string;
+    enddate: string;
+    url: string;
+    urlbase: string;
+    copyright: string;
+    copyrightlink: string;
+    title: string;
+    quiz: string;
+    wp: boolean;
+    hsh: string;
+    [key: string]: any;
+  }[];
   [key: string]: any;
 }
